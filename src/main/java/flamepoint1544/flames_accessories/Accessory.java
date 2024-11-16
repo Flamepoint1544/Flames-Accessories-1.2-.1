b64bb6c594e42067e17a27e5d1c993c5d8956473
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-11-16 14:14:45 -0700

    added inital code

---
+++ src/main/java/flamepoint1544/flames_accessories/Accessory.java
@@ -0,0 +1,9 @@
+package flamepoint1544.flames_accessories;
+
+import dev.emi.trinkets.api.TrinketItem;
+
+public class Accessory extends TrinketItem{
+    public Accessory(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/flamepoint1544/flames_accessories/ModItems.java
@@ -0,0 +1,52 @@
+package flamepoint1544.flames_accessories;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // item group registry keys
+    public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FlamesAccessories.MOD_ID, "primary_item_group"));
+
+    // Item Groups
+    public static final ItemGroup PRIMARY_ITEM_GROUP = FabricItemGroup.builder()
+    .icon(() -> new ItemStack(Items.BEDROCK.asItem()))
+    .displayName(Text.translatable("itemGroup.flames_accessories.primary_item_group")).build();
+    
+    /***************************************************************************************************
+     * +# register(Item item, String id): Item
+     * Registers items using the string id and the modid defined in the main class to make an identifier
+     ***************************************************************************************************/
+    public static Item register(Item item, String id){
+        // Create Identifier
+        Identifier itemID = Identifier.of(FlamesAccessories.MOD_ID, id);
+
+        // Register the item
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return
+        return registeredItem;
+    }
+
+    /**********************************************************
+     * +# initialize(): void
+     * Initializes the file while also registering item groups
+     *********************************************************/
+    public static void initialize(){
+        // Register group
+        Registry.register(Registries.ITEM_GROUP, PRIMARY_GROUP_KEY, PRIMARY_ITEM_GROUP);
+
+        // add items to registered group
+        ItemGroupEvents.modifyEntriesEvent(PRIMARY_GROUP_KEY).register(itemGroup ->{
+            itemGroup.add(Items.GOLD_INGOT);
+         });
+    }
+}
