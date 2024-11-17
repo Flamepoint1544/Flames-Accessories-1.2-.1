ba61d3d9693eb10b988b8fc5d0d9248c7761c764
Flamepoint1544 <Flamepoint1544@gmail.com>
2024-11-16 18:04:36 -0700

    added speed ult accessory

---
+++ src/main/java/flamepoint1544/flames_accessories/AccessoryMult.java
@@ -0,0 +1,39 @@
+package flamepoint1544.flames_accessories;
+
+import java.util.UUID;
+
+import com.google.common.collect.Multimap;
+
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ItemStack;
+
+public class AccessoryMult extends TrinketItem{
+    private final EntityAttribute[] attribute;
+    private final String[] modifierName;
+    private final double[] value;
+    private final EntityAttributeModifier.Operation[] operation;
+    private final int count;
+
+    public AccessoryMult(Settings settings, EntityAttribute[] attribute, String[] modifierName, double[] value, EntityAttributeModifier.Operation[] operation, int count) {
+        super(settings);
+        this.attribute = attribute;
+        this.modifierName = modifierName;
+        this.value = value;
+        this.operation = operation;
+        this.count = count;
+    }
+
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid){
+        var modifiers = super.getModifiers(stack, slot, entity, uuid);
+
+        for(int i = 0; i < count; i++){
+            modifiers.put(attribute[i], new EntityAttributeModifier(uuid, modifierName[i], value[i], operation[i]));
+        }
+
+        return modifiers;
+    }
+}
--- src/main/java/flamepoint1544/flames_accessories/ModItems.java
+++ src/main/java/flamepoint1544/flames_accessories/ModItems.java
@@ -13,10 +13,18 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.entity.attribute.EntityAttribute;
 
 public class ModItems {
+    private static final EntityAttribute[] zephyrAttributes = new EntityAttribute[]{EntityAttributes.GENERIC_MOVEMENT_SPEED, EntityAttributes.GENERIC_MOVEMENT_SPEED};
+    private static final String[] zephyrAttributeNames = new String[]{"flames_accessories:movement_speed_ult", "flames_accessories:attack_speed_alt"};
+    private static final double[] zephyrValues = new double[]{0.2, 3};
+    private static final EntityAttributeModifier.Operation[] zephyrOperations = new EntityAttributeModifier.Operation[]{EntityAttributeModifier.Operation.MULTIPLY_TOTAL, EntityAttributeModifier.Operation.ADDITION};
+
     // Items
     public static final Item SPEED_CHARM = register(new Accessory(new Item.Settings(), EntityAttributes.GENERIC_MOVEMENT_SPEED, "flames_accessories:movement_speed", 0.05, EntityAttributeModifier.Operation.MULTIPLY_TOTAL), "speed_charm");
+    public static final Item SPEED_CHARM_T2 = register(new Accessory(new Item.Settings(), EntityAttributes.GENERIC_MOVEMENT_SPEED, "flames_accessories:movement_speed", 0.2, EntityAttributeModifier.Operation.MULTIPLY_TOTAL), "speed_charm_t2");
+    public static final Item SPEED_CHARM_ULT = register(new AccessoryMult(new Item.Settings(), zephyrAttributes, zephyrAttributeNames, zephyrValues, zephyrOperations, 2), "speed_charm_ult");
 
     // item group registry keys
     public static final RegistryKey<ItemGroup> PRIMARY_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FlamesAccessories.MOD_ID, "primary_item_group"));
@@ -52,6 +60,8 @@ public class ModItems {
         // add items to registered group
         ItemGroupEvents.modifyEntriesEvent(PRIMARY_GROUP_KEY).register(itemGroup ->{
             itemGroup.add(SPEED_CHARM);
+            itemGroup.add(SPEED_CHARM_T2);
+            itemGroup.add(SPEED_CHARM_ULT);
          });
     }
 }
